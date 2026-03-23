ff25595bfa4c083c87b2c7053703e731e7cdf018
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-23 18:01:40 -0300

    feat: add docker and db

---
+++ backend/src/central-de-manutencao.Api/Database/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using central_de_manutencao.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace central_de_manutencao.Api.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ backend/src/central-de-manutencao.Api/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using central_de_manutencao.Api.Enums;
+
+namespace central_de_manutencao.Api.Models
+{
+
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public Roles role { get; set; }
+        public Specialties? specialty { get; set; }
+        public bool active { get; set; }
+        public DateTime createdAt { get; set; }
+    }
+}
