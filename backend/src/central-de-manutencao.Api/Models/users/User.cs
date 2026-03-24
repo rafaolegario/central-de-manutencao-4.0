37bef4a909ba5885974aeef6b3acd9aec26fdaee
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-23 21:21:58 -0300

    refactor: rename User properties to PascalCase and move to Users namespace

---
+++ backend/src/central-de-manutencao.Api/Models/users/User.cs
@@ -0,0 +1,16 @@
+﻿using central_de_manutencao.Api.Enums;
+
+namespace central_de_manutencao.Api.Models.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public Roles Role { get; set; }
+        public Specialties? Specialty { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
