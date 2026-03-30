2f2da19e565d94c4808b2ec41cdefdf5988702d9
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-30 17:46:12 -0300

    refactor: update AuthenticateRequestJson

--- backend/src/central-de-manutencao.Communication/Requests/Auth/AuthenticateRequestJson.cs
+++ backend/src/central-de-manutencao.Communication/Requests/Auth/AuthenticateRequestJson.cs
@@ -2,7 +2,7 @@ namespace central_de_manutencao.Communication.Requests.Auth
 {
     public class AuthenticateRequestJson
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
