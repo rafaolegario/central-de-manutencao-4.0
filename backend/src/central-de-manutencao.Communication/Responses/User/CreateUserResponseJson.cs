dd38c61ec7baaf2437adfcf43b4a21cdb68fb487
rafaolegario <rafaelbisofficel@gmail.com>
2026-03-30 17:47:11 -0300

    feat: add CreateUserResponseJson DTO

---
+++ backend/src/central-de-manutencao.Communication/Responses/User/CreateUserResponseJson.cs
@@ -0,0 +1,11 @@
+namespace central_de_manutencao.Communication.Requests.User
+{
+    public class CreateUserResponseJson
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Specialty { get; set; } = string.Empty;
+    }
+
+}
