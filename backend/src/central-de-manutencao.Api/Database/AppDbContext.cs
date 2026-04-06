b2f989f3f93f0947e807898c208aab7af36231eb
rafaolegario <rafaelbisofficel@gmail.com>
2026-04-06 18:20:04 -0300

    feat: add ServiceOrder DbSets with indexes and concurrency config

--- backend/src/central-de-manutencao.Api/Database/AppDbContext.cs
+++ backend/src/central-de-manutencao.Api/Database/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using central_de_manutencao.Api.Models.Users;
+using central_de_manutencao.Api.Models.ServiceOrders;
 using Microsoft.EntityFrameworkCore;
 
 namespace central_de_manutencao.Api.Database
@@ -8,5 +9,22 @@ namespace central_de_manutencao.Api.Database
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
 
         public DbSet<User> Users { get; set; }
+        public DbSet<ServiceOrder> ServiceOrders { get; set; }
+        public DbSet<ServiceOrderLog> ServiceOrderLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ServiceOrder>(entity =>
+            {
+                entity.Property(e => e.RowVersion)
+                    .IsRowVersion();
+
+                entity.HasIndex(e => e.Status);
+                entity.HasIndex(e => e.CreatedAt);
+                entity.HasIndex(e => e.TechnicianId);
+            });
+        }
     }
 }
