a9468edcd23e07626752f1200d881082b17dcfa8
otavio <otaviotemoteo@gmail.com>
2026-04-16 01:22:42 -0300

    fix: add CORS configuration to backend API

--- backend/src/central-de-manutencao.Api/Program.cs
+++ backend/src/central-de-manutencao.Api/Program.cs
@@ -17,6 +17,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddScoped<ITokenProvider, HttpContextTokenValue>();
 builder.Services.AddHttpContextAccessor();
 
@@ -98,6 +108,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHttpsRedirection();
 }
 
+app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 
