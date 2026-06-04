cacbef94daa9f9d854f4495c83f3a0da24ac3316
otavio <otaviotemoteo@gmail.com>
2026-06-03 21:10:24 -0300

    feat(db): migration add StockMovement.CreatedBy

---
+++ backend/src/central-de-manutencao.Api/Migrations/20260603234200_AddCreatedByToStockMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace central_de_manutencao.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedByToStockMovement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "StockMovements",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "StockMovements");
+        }
+    }
+}
